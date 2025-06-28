061591a13c20f56d01e296c2381758328145e39f
markmur90 <markmur90@proton.me>
2025-06-28 23:54:28 +0200

    dele

--- Procfile
+++
@@ -1,4 +0,0 @@
-release: python3 manage.py migrate
-release: python3 manage.py collectstatic --noinput
-
-web: gunicorn config.wsgi:application --log-file -
--- api/authentication/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from api.authentication.models import CustomUser
-
-# Register your models here.
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'phone', 'is_staff')
-    search_fields = ('username', 'email', 'phone')
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
