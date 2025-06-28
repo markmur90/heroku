4406da1bfde74b0903d7f1c2b52794439df746c6
markmur90 <markmur90@proton.me>
2025-06-29 00:48:59 +0200

    sc

--- middleware/oficial_session.py
+++
@@ -1,8 +0,0 @@
-class DetectarOficialMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        if request.user.is_authenticated and request.user.username == "oficial_cuentas":
-            request.session["usar_conexion_banco"] = "oficial"
-        return self.get_response(request)
