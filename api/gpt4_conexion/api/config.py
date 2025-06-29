c2c88de6e7f81a8e70d3e671a09aeb05c32d6523
markmur90 <markmur90@proton.me>
2025-06-29 02:56:21 +0200

    sc

--- api/gpt4_conexion/api/config.py
+++
@@ -1,9 +0,0 @@
-# api/config.py
-from django.conf import settings
-
-BANK_API_TOKEN = settings.BANK_API_TOKEN
-TRANSFER_ENDPOINT = f"/api/transferencia"
-HEADERS = {
-    'Authorization': f"Bearer {BANK_API_TOKEN}",
-    'Content-Type': 'application/json'
-}
