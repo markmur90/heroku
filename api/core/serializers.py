197bc8e06992a970e15f515ebc983f9cbc71e5b3
markmur90 <markmur90@proton.me>
2025-06-30 07:32:47 +0200

    DEL

--- api/core/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from api.core.models import CoreModel  # Asegúrate de que CoreModel esté definido en models.py
-
-class CoreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CoreModel
-        fields = '__all__'
