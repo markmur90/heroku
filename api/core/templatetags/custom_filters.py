0fe5b89a26508021a1b3e813fe60a1b583dc1a9b
markmur90 <markmur90@proton.me>
2025-07-07 07:14:18 +0200

    dl

--- api/core/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-# core/templatetags/custom_filters.py
-from django import template
-
-register = template.Library()
-
-@register.filter
-def startswith(text, prefix):
-    return text.startswith(prefix)
