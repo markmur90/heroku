dd5c12b03a913f1c291310d758aad0c3d7cf0d74
markmur90 <markmur90@proton.me>
2025-07-07 04:54:59 +0200

    sc

--- api/authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from api.authentication.views import LoginView, UserProfileView
-
-urlpatterns = [
-    path('login/', LoginView.as_view(), name='login_api'),
-    path('profile/', UserProfileView.as_view(), name='profile_api'),
-]
